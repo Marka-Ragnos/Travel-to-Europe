3bf287d8f23409c5d54457121da86928e73816f1
Саня <dunkan-1@yandex.ru>
2020-12-23 10:26:29 +0300

    рефакторинг

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,31 @@
 "use strict";
+
+// плавная прокрутка
+const anchors = [].slice.call(document.querySelectorAll(".main-nav__link")),
+   animationTime = 300,
+   framesCount = 20;
+
+anchors.forEach(function (item) {
+   item.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const coordY =
+         document
+            .querySelector(item.getAttribute("href"))
+            .getBoundingClientRect().top + window.pageYOffset;
+
+      const scroller = setInterval(function () {
+         const scrollBy = coordY / framesCount;
+
+         if (
+            scrollBy > window.pageYOffset - coordY &&
+            window.innerHeight + window.pageYOffset < document.body.offsetHeight
+         ) {
+            window.scrollBy(0, scrollBy);
+         } else {
+            window.scrollTo(0, coordY);
+            clearInterval(scroller);
+         }
+      }, animationTime / framesCount);
+   });
+});
